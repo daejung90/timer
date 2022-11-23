06eeefa6ecaf436275f58d0886662eb4fd402140
daejung90 <djung.ca@gmail.com>
2022-11-23 14:21:34 -0500

    if interval is negative number statement added

--- timer1.js
+++ timer1.js
@@ -5,11 +5,15 @@ let interval = input
 let alarm = function (){
     
     for (let i = 0; i < interval.length; i++){
+        if(interval[i] < 0) {
+            interval[i]++
+        } else {
     setTimeout(() => {
         // console.log(interval)
         process.stdout.write('BEEP\n')
     },interval[i] * 1000)
 
+    }
   }
 }
 alarm()
